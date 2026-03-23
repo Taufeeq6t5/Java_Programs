361217e4909c59851edb796a8217ad46bcba3862
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-03-23 15:12:00 +0530

    Added ArraySort.java
    A simple Java program to sort an array in ascending order using Bubble Sort.

--- ArraySort.java
+++ ArraySort.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+public class ArraySort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        // Input elements
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Output sorted array
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
