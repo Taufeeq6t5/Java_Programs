94c618d1b6ff5c67c43e9ab1bb9aa2dab5827a8a
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-02 11:58:38 +0530

    Implement basic shopping cart functionality

--- ShoppingCartPrgm.java
+++ ShoppingCartPrgm.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class ShopingCartPrgm{
+    
+    public static void main (String args[]){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        String item;
+        double price;
+        int quantity;
+        double total;
+        
+        System.out.print("What item do you need to buy?: ");
+        item=sc.nextLine();
+        
+        System.out.print("What is the price of the item?: ");
+        price=sc.nextDouble();
+        
+        System.out.print("How much quantity do you need?: ");
+        quantity=sc.nextInt();
+        
+        total=price*quantity;
+        System.out.print("The total Price is " + total + " rs");
+        
+        sc.close();
+    }
+}
