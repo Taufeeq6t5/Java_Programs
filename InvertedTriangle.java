f45f28f0eeeed689888e8f0a3fdf38b44b743aeb
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-13 13:25:19 +0530

    Implement inverted triangle pattern printing
    Add a program to print an inverted triangle pattern based on user input.

--- InvertedTriangle.java
+++ InvertedTriangle.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class InvertedTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {        // rows decreasing
+            for (int j = 1; j <= i; j++) {    // print stars
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
