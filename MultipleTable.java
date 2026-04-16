6879aedf835fd02f28c515612807703e246c1e6d
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-16 08:12:39 +0530

    Implement multiplication table generator
    Add a program to print multiplication tables up to a user-defined limit.

--- MultipleTable.java
+++ MultipleTable.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class MultipleTables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter limit: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("\nTable of " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+        }
+        sc.close();
+    }
+}
