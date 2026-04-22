1a824fee0a3a0963c42f3e6cbf8b622817c27f93
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-22 09:43:54 +0530

    Fix formatting in PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -10,7 +10,7 @@ public class PalindromeNumber {
 
         int original = num;
         int reverse = 0;
-
+        
         while (num != 0) {
             int digit = num % 10;
             reverse = reverse * 10 + digit;
