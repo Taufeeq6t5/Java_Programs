562094e9af4cd83ea1af984362c2e32d8cf696de
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-07-29 22:05:16 +0530

    Implement temperature conversion functionality
    Added a temperature conversion feature that allows users to convert between Celsius and Fahrenheit using user input.

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class TemperatureConverter{
+
+    public static void main (String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+       double temp;
+       double newTemp;
+       String unit;
+
+        System.out.print("Enter the temperature: ");
+        temp = sc.nextDouble();
+
+        System.out.print("Convert to Celcius or Fahrenheit? (C or F): ");
+        unit = sc.next().toUpperCase();
+
+      //Using Ternary operator ("?") 
+        newTemp = (unit.equals("C")) ? (temp - 32) *  5 / 9 : (temp * 5 / 9) + 32;
+
+        System.out.println(newTemp);
+
+        sc.close();
+    }
+}
